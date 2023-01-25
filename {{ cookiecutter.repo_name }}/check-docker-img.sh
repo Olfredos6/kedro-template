c7da505045068fdb4d1c9cf25b4466156d437ea4
Nehemie Balukidi <nehemie.balukidi@dae.mn>
2023-01-25 10:17:40 +0200

    fix: in-container app directory

--- {{ cookiecutter.repo_name }}/check-docker-img.sh
+++ {{ cookiecutter.repo_name }}/check-docker-img.sh
@@ -1,6 +1,14 @@
+ROOTPATH=`dirname $(readlink -f $0)`
+echo $ROOTPATH
+echo $ROOTPATH"/docker"
 img_name="docker-kedro:latest"
 dockerfile_path=spaceflights/docker
+cd $ROOTPATH"/docker"
+echo "haaaaaaaaaaa!"
 if [[ "$(docker images -q $img_name  2> /dev/null)" == "" ]]
 then
-    docker build -t $img_name spaceflights/
+    docker compose up --build
+else
+    docker compose down
+    docker compose up
 fi
